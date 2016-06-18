e405a86e7e020f5f25e1c05aae68fa7d560400c1
Hardys <hardyscc@gmail.com>
2016-06-18 14:46:51 +0800

    use liblinear-2.1 for finalClassifier

--- setup.m
+++ setup.m
@@ -6,6 +6,7 @@ addpath(fullfile(cd,'src','forest'));
 addpath(fullfile(cd,'src','seg-enc'));
 addpath(fullfile(cd,'lib','matlab-tree'));
 addpath(fullfile(cd,'lib','graphSeg'));
+addpath(fullfile(cd,'lib','liblinear-2.1','matlab'));
 addpath(fullfile(cd,'lib','liblinear-multicore-2.1-2','matlab'));
 run('/opt/vlfeat/toolbox/vl_setup');
 

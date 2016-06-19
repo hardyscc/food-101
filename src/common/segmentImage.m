6e86380497a895921305c800dd7b5f896e257a73
Tommy Chong <tommy@sysforce.com>
2016-06-19 17:05:02 +0800

    add matlab binary for mac osx

--- src/common/segmentImage.m
+++ src/common/segmentImage.m
@@ -1,15 +1,19 @@
-function L = segmentImage(I)
+function L = segmentImage(I, computeColorOutput)
 %segmentImage Perform segmentation on an image using the Graph-Based
 %Segmentation algorithm by Felzenszwalb
 %   I: The image to be segmented
 %   L: The produced segments
+
+if nargin < 2
+    computeColorOutput = false;
+end
 
 [height, width, ~] = size(I);
 sigma = 0.1;
 kappa = 300;
 minSize = height * width * 0.01;
 
-L = segmentFelzenszwalb(I, sigma, kappa, minSize) + 1;
+L = segmentFelzenszwalb(I, sigma, kappa, minSize, computeColorOutput) + 1;
 
 end
 
